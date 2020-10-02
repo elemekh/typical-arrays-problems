30e9cae0230e388779e383efe7d4bf4f7b568bc5
Evgeny Lemekh <elemekh@gmail.com>
2020-10-03 02:13:32 +0300

    init: start test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (arguments.length !== 0 && array.length !== 0) {
+      return Math.min(...array);
+  }
   return 0;
-}
+};
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (arguments.length !== 0 && array.length !== 0) {
+      return Math.max(...array);
+  }
   return 0;
-}
 
-exports.avg = function avg (array) {
-  return 0;
+};
+
+exports.avg = function avg(array) {
+  if (arguments.length !== 0 && array.length !== 0) {
+      return array.reduce((a, b) => (a + b)) / array.length;
+  } else {
+      return 0;
+  }
 }
